75cfdbfde32178543463d249fdc29b34054760d9
Lucas Marques <cmarques.lucas@gmail.com>
2017-06-05 09:22:11 -0300

    wholesaler app creation

---
+++ wholesaler/src/main/java/com/productpurchases/shopkeeper/wholesaler/WholesalerApplication.java
@@ -0,0 +1,16 @@
+package com.productpurchases.shopkeeper.wholesaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lucascmarques on 04/06/17.
+ */
+@SpringBootApplication
+public class WholesalerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WholesalerApplication.class, args);
+    }
+
+}
