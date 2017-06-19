45cf5772a64320cba010904ad171dfe42bab945f
Arthur Mourão <arthur.felipem@gmail.com>
2017-06-18 22:00:24 -0300

    Including Swagger

--- wholesaler/src/main/java/com/productpurchases/wholesaler/WholesalerApplication.java
+++ wholesaler/src/main/java/com/productpurchases/wholesaler/WholesalerApplication.java
@@ -2,6 +2,13 @@ package com.productpurchases.wholesaler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * Created by lucascmarques on 04/06/17.
@@ -11,6 +18,31 @@ public class WholesalerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WholesalerApplication.class, args);
+    }
+
+	@Bean
+	public Docket swaggerSettings() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()
+	                .apis(RequestHandlerSelectors.any())
+	                .paths(PathSelectors.any())
+	                .build().apiInfo(apiInfo())
+	                .pathMapping("/");
+	    }	
+	 
+	/**
+     * API Info as it appears on the swagger-ui page
+     */
+    private ApiInfo apiInfo() {
+        	    ApiInfo apiInfo = new ApiInfo(
+        	    	      "API Wholesaler",
+        	    	      "API Wholesaler - Trabalho Final Backend",
+        	    	      "1.0",
+        	    	      "Terms of service",
+        	    	      "a@gmail.com",
+        	    	      "Apache 2.0",
+        	              "http://www.apache.org/licenses/LICENSE-2.0.html");
+        return apiInfo;
     }
 
 }
